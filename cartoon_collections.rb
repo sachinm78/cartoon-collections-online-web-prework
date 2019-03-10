1b94cd7434c8a47f31d7768090562471046dff70
Sachin Mahajan <sachinm78@gmail.com>
2019-03-10 22:52:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,12 +21,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
- i = 0 
- if array.any? {|i| i.length > 4}
+  i = 0 
+  if array.any? {|i| i.length > 4}
    return true
   else
     return false 
-  i += 1
+  i = i + 1
   end
 end
 
