ace3b20245cc431b54aaee3a68194c7a02aab576
Sachin Mahajan <sachinm78@gmail.com>
2019-03-10 22:53:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,13 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  nu_array = []
+  new_array = []
   i = 0 
   while i < array.length 
-    nu_array << array[i].capitalize + "!"
+    new_array << array[i].capitalize + "!"
     i += 1 
   end 
-  nu_array
+  new_array
 end
 
 def long_planeteer_calls(array)
